99a365b0bcac2037b8a8f1d332b1136d3d2c87f5
Jesus C Guerrero <jesusclementguerrero@gmail.com>
2020-04-14 19:33:56 -0400

    mvp

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,21 @@
 import React from 'react';
+import { addFeature } from '../actions/AddFeature'
+import { connect } from 'react-redux'
 
 const AdditionalFeature = props => {
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button onClick={() => props.addFeature(props.feature)} className="button">Add</button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+const mapStateToProps = state => {
+  return {
+    features: state.car.features
+  }
+}
+
+export default connect(mapStateToProps, { addFeature })(AdditionalFeature);
